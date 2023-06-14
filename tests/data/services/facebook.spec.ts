5489e64f376fc2294dc56113a8bacb6a486a42be
GiovannaK <giovannakelli@gmail.com>
2023-06-13 23:27:20 -0300

    chore: create token variable

--- tests/data/services/facebook.spec.ts
+++ tests/data/services/facebook.spec.ts
@@ -6,21 +6,23 @@ import { mock, type MockProxy } from 'jest-mock-extended'
 describe('FacebookAuthenticationService', () => {
   let loadFacebookUserApi: MockProxy<LoadFacebookUserApi>
   let sut: FacebookAuthenticationService
+  const token = 'any_token'
+
   beforeEach(() => {
     loadFacebookUserApi = mock<LoadFacebookUserApi>()
     sut = new FacebookAuthenticationService(loadFacebookUserApi)
   })
   it('should call loadFacebookApi with correct params', async () => {
-    await sut.perform({ token: 'any_token' })
+    await sut.perform({ token })
 
-    expect(loadFacebookUserApi.loadUser).toHaveBeenCalledWith({ token: 'any_token' })
+    expect(loadFacebookUserApi.loadUser).toHaveBeenCalledWith({ token })
     expect(loadFacebookUserApi.loadUser).toHaveBeenCalledTimes(1)
   })
 
   it('should return AutheticationError when loadFacebookApi returns undefined', async () => {
     loadFacebookUserApi.loadUser.mockResolvedValueOnce(undefined)
 
-    const authResult = await sut.perform({ token: 'any_token' })
+    const authResult = await sut.perform({ token })
 
     expect(authResult).toEqual(new AuthenticationError())
   })
